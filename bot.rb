8f1b2c54be1148965598d507fdd4a61d27dddc90
serenityzn <serenity.zn@gmail.com>
2014-05-23 19:01:07 +0300

    added check block logic

--- bot.rb
+++ bot.rb
@@ -1,7 +1,7 @@
 class Bot
 	def initialize(window)
 	 @image = Gosu::Image.new(window, "imgs/bot1.png", false)
-	 @x = @y = @vel_y = @vel_x = @angle = 0.0
+	 @x = @y = @x0 = @y0 = @vel_y = @vel_x = @angle = 0.0
 	 @score = 0
 	 @step = 1
 	 @blk = Array.new
@@ -26,6 +26,42 @@ class Bot
 	 norm = [v[0]/len, v[1]/len]
 	 return norm
 	end
+
+	def norm(v)
+	    v1 = Array.new
+	    v1[0] = v[0]/len(v)
+	    v1[1] = v[1]/len(v)
+	    return v1
+	end
+
+	def skal(a,b)
+	 res = a[0]*b[0]+a[1]*b[1]
+	 return res
+	end
+
+	def crossblock(direction, x, bl)
+	        res = true
+	        bl1 =[0,0]
+	        bl1[0] = bl[0]
+	        bl1[1] = bl[1]+20
+	        print bl.to_s+"\n"
+	        print bl1.to_s+"\n"
+	        v1 = [bl[0]-x[0], bl[1]-x[1]]
+	        v2 = [bl1[0]-x[0], bl1[1]-x[1]]
+	        v1_v2 = (Math.acos(skal(norm(v1), norm(v2)))*180/3.14159)
+	        v1_x = (Math.acos(skal(norm(v1), norm(direction)))*180/3.14159)
+	        v2_x = (Math.acos(skal(norm(v2), norm(direction)))*180/3.14159)
+	#        print "Angle V1 with V2 ="+v1_v2.to_s+"\n"
+	 #       print "Angle V1 with X ="+v1_x.to_s+"\n"
+	  #      print "Angle V1 with X ="+v2_x.to_s+"\n"
+	        if v1_x > v1_v2
+	         res = false
+	        elsif v2_x > v1_v2
+	         res = false
+	        end
+
+	        return res
+	end
 	
 	def chg_dir(x,y)
 	 v = [x-@x, y-@y]
@@ -48,11 +84,16 @@ class Bot
 	 if (check_fov1(x,y,120) == 1 and block_fov(x,y) == 1)
 	  @direction=chg_dir(x,y)
 	 end
-		if simple_check == 0
+		if simple_check(@direction) == 0
+		 @x0 = @x
+		 @y0 = @y
 		 @x += @direction[0]
 		 @y += @direction[1]
 		else
-		 @direction[0] = -1*@direction[0]
+#		 @direction[0] = -1*@direction[0]
+		 @direction = chg_dir(@x0, @y0)
+		 @x = @x0
+		 @y = @y0
 		end
 	end
 	
@@ -81,7 +122,6 @@ class Bot
 	end
 
 	def showxy(x,y)
-#	 print "Direction=["+@direction[0].to_s+","+@direction[1].to_s+"]\n"
 	end
 
 	def draw
@@ -147,13 +187,18 @@ class Bot
 	 return see
 	end
 
-	def simple_check
+	def simple_check(v_n)
+	 step = 20
 	 i = 0
 	 res = 0
+	 x = [@x, @y]
 	 while i < @blk[0].size
-	  if len(vroute(@x+@direction[0],@y+@direction[1],@blk[0][i],@blk[1][i])) < 40
-		res = 1
-	  end
+	  bl = [@blk[0][i], @blk[1][i]]
+	  if crossblock(@direction, x, bl ) == true
+	   if len(vroute(@x+@direction[0],@y+@direction[1],@blk[0][i],@blk[1][i])) < step
+	 	res = 1
+	   end
+          end
 	  i+=1
 	 end
 	 return res
--- test.rb
+++ test.rb
@@ -1,21 +1,47 @@
-def readlvl(lvl)
- bloks = Array.new
- bloks[0] = Array.new
- bloks[1] = Array.new
- i=0
- j=0
- open(lvl).each {|f|
-   while j<32
- 	if f[j] == "x"
- 	 bloks[0]<< 20*j
- 	 bloks[1]<<20*(i-1)
- 	end
- 	j+=1
-   end
- i+=1
- j=0
- }
-
- return bloks
+def norm(v)
+    v1 = Array.new
+    v1[0] = v[0]/len(v)
+    v1[1] = v[1]/len(v)
+    return v1
 end
+
+def len(v)
+ res =Math.sqrt(v[0]*v[0]+v[1]*v[1])
+ return res
+end
+
+def skal(a,b)
+ res = a[0]*b[0]+a[1]*b[1]
+ return res
+end
+
+def crossblock(direction, x, bl)
+	res = true
+	bl1 =[0,0]
+	bl1[0] = bl[0]
+	bl1[1] = bl[1]+20
+	print bl.to_s+"\n"
+	print bl1.to_s+"\n"
+	v1 = [bl[0]-x[0], bl[1]-x[1]]
+	v2 = [bl1[0]-x[0], bl1[1]-x[1]]
+	v1_v2 = (Math.acos(skal(norm(v1), norm(v2)))*180/3.14159)
+	v1_x = (Math.acos(skal(norm(v1), norm(direction)))*180/3.14159)
+	v2_x = (Math.acos(skal(norm(v2), norm(direction)))*180/3.14159)
+	print "Angle V1 with V2 ="+v1_v2.to_s+"\n"
+	print "Angle V1 with X ="+v1_x.to_s+"\n"
+	print "Angle V1 with X ="+v2_x.to_s+"\n"
+	if v1_x > v1_v2
+	 res = false
+	elsif v2_x > v1_v2
+	 res = false
+	end
+	
+	return res
+end
+
+direction = [0, -1]
+x = [5,7]
+bl = [10,1]
+
+print crossblock(direction, x, bl)
 
