a0ca2e963bb7652cb4f048bdc0a8c3fc973cb255
serenityzn <serenity.zn@gmail.com>
2014-05-23 19:04:21 +0300

    added check block logic1

--- bot.rb
+++ bot.rb
@@ -44,16 +44,11 @@ class Bot
 	        bl1 =[0,0]
 	        bl1[0] = bl[0]
 	        bl1[1] = bl[1]+20
-	        print bl.to_s+"\n"
-	        print bl1.to_s+"\n"
 	        v1 = [bl[0]-x[0], bl[1]-x[1]]
 	        v2 = [bl1[0]-x[0], bl1[1]-x[1]]
 	        v1_v2 = (Math.acos(skal(norm(v1), norm(v2)))*180/3.14159)
 	        v1_x = (Math.acos(skal(norm(v1), norm(direction)))*180/3.14159)
 	        v2_x = (Math.acos(skal(norm(v2), norm(direction)))*180/3.14159)
-	#        print "Angle V1 with V2 ="+v1_v2.to_s+"\n"
-	 #       print "Angle V1 with X ="+v1_x.to_s+"\n"
-	  #      print "Angle V1 with X ="+v2_x.to_s+"\n"
 	        if v1_x > v1_v2
 	         res = false
 	        elsif v2_x > v1_v2
